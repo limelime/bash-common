a2de0d454d036013171f07a4722f1b89b48acaeb
limelime <limelime@users.noreply.github.com>
2017-02-10 17:22:03 -0500

    Update label.

--- lib/func_link.sh
+++ lib/func_link.sh
@@ -1,4 +1,4 @@
-# Description: Create soft symbolic link: TARGET -> LINK
+# Description: Create soft symbolic link: TARGET_NAME -> LINK_NAME
 
 function func_link()
 { # Warning: LINK_NAME will be destroyed. 
@@ -6,7 +6,7 @@ function func_link()
   local LINK_NAME=$2
   if [ $# -ne 2 ]; then
     echo "Error: Missing arguments. Aborted!"
-    echo "  ${FUNCNAME[0]} TARGET LINK_NAME"
+    echo "  ${FUNCNAME[0]} TARGET_NAME LINK_NAME"
     echo "  ${FUNCNAME[0]} /backup/data/google-chrome  /root/.config/google-chrome"
     return 1;
   fi
