0b70baab5e696cee98325be8287c293290f4a62c
limelime <limelime@users.noreply.github.com>
2017-02-10 18:25:10 -0500

    Add apache link.

---
+++ lib/func_apache_www_link.sh
@@ -0,0 +1,37 @@
+# Description: XXXX
+#   -Depends on: XXXX
+
+function func_apache_www_link()
+{ 
+  local WEB_ROOT_DIR=$1
+  local APACHE_SYMBOLIC_NAME=$2
+  
+  # Error handling
+    if [ $# -ne 2 ]; then
+      echo "${FUNCNAME[0]}: Error: Missing arguments. Aborted!"
+      echo "   e.g.: ${FUNCNAME[0]}  WEB_ROOT_DIR APACHE_SYMBOLIC_NAME"
+      echo "   e.g.: ${FUNCNAME[0]}  /path/to/drupal/ opw"
+      return 1;
+    fi
+    
+    if [ ! -d ${WEB_ROOT_DIR} ]; then
+      echo "${FUNCNAME[0]}: Error: ${WEB_ROOT_DIR} is not a directory."
+      return 1;
+    fi
+  
+    WEB_ROOT_DIR=$(readlink -ev "${WEB_ROOT_DIR}")
+  
+  # Link apache to folder
+    APACHE_DIR=/var/www/html/${APACHE_SYMBOLIC_NAME}
+    rm -f ${APACHE_DIR}
+    ln -s ${WEB_ROOT_DIR} ${APACHE_DIR}
+    #chmod -R 777 ${WEB_ROOT_DIR}
+    chown -R www-data:www-data ${WEB_ROOT_DIR}
+    chmod -R 755 ${WEB_ROOT_DIR}
+  
+  # Display symbolic links
+    echo "${FUNCNAME[0]}: Linked ${APACHE_SYMBOLIC_NAME} -> ${WEB_ROOT_DIR}"    
+}  
+export -f func_apache_www_link
+
+# Test: XXXX
