01ed781b0fecc263de05ca98a3e09eff85b9eee6
limelime <limelime@users.noreply.github.com>
2017-02-14 16:18:36 -0500

    Refactor.

--- func-list.sh
+++ func-list.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
-# Description:
+# Description: Load all *.sh from lib/
 
 LIB_DIR=$(readlink -ev ./lib)
-#find "${LIB_DIR}" -maxdepth 1 -name '*.sh' -execdir . {} \;
+source <(cat ${LIB_DIR}/*.sh)
 
-#source <(cat ${LIB_DIR}/*.sh)
-#for f in ${LIB_DIR}/*.sh; do source $f; done
-
-source ${LIB_DIR}/func_apache_www_link.sh
-source ${LIB_DIR}/func_backup.sh
-source ${LIB_DIR}/func_bashrc_add_path.sh
-source ${LIB_DIR}/func_bashrc_add.sh
-source ${LIB_DIR}/func_dir_get_executing_path.sh
-source ${LIB_DIR}/func_get_current_dir.sh
-source ${LIB_DIR}/func_link.sh
-source ${LIB_DIR}/func_id.sh
