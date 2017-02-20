4cc078af90cfa7ba4f590fc768786ab6e105526f
limelime <limelime@users.noreply.github.com>
2017-02-20 16:37:55 -0500

    Fix sed: remove extension(.sh).

--- func-test-create.sh
+++ func-test-create.sh
@@ -24,7 +24,7 @@ FUNCTION_FILE=$1
     echo "./test-${FUNCTION_NAME}" >> "${TESTS_DIR}/run.sh"
         
     # Create function test file.
-    FUNCTION_NAME_NO_EXT=$(echo "${FUNCTION_NAME}" | sed 's/.sh//')
+    FUNCTION_NAME_NO_EXT=$(echo "${FUNCTION_NAME}" | sed 's/.sh$//')
     sed "s/FUNCTION_NAME/${FUNCTION_NAME_NO_EXT}/" "${TESTS_DIR}/template-test.sh" > "${FUNCTION_TEST_FILE}"
     chmod +x "${FUNCTION_TEST_FILE}"
     
