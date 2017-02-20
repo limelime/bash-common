a8e3fff09ee40db98f274edabdf15f9b3e7ba503
limelime <limelime@users.noreply.github.com>
2017-02-20 16:07:06 -0500

    Source correctly.

--- func-list-load.sh
+++ func-list-load.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
-# Description: Load all *.sh from lib/
+# Description: Load all *.sh from source lib/
 
 LIB_DIR=$(readlink -ev ./lib)
-source <(cat ${LIB_DIR}/*.sh)
+#source <(cat ${LIB_DIR}/*.sh) # Cause an error to close current shell.
+#find "${LIB_DIR}" -maxdepth 1 -name 'func_*.sh' -execdir echo "sourced ${LIB_DIR}/{}" \; -execdir /bin/bash -c "source ${LIB_DIR}/{}" \; 
 
+source lib/func_apache_www_link.sh
+source lib/func_backup.sh
+source lib/func_bashrc_add_path.sh
+source lib/func_bashrc_add.sh
+source lib/func_cmd_exists.sh
+source lib/func_dir_get_executing_path.sh
+source lib/func_id.sh
+source lib/func_link.sh
