fec203385e7fb3fc79a612303a708337da8beba2
limelime <limelime@users.noreply.github.com>
2017-02-20 14:48:13 -0500

    Don't create if file already exists.

--- func-create.sh
+++ func-create.sh
@@ -11,9 +11,15 @@ FUNCTION_NAME=$1
   fi
 
 # Create function file.
-LIB_DIR=$(readlink -ev ./lib)
-FUNCTION_NAME="func_${FUNCTION_NAME}"
-sed "s/FUNCTION_NAME/${FUNCTION_NAME}/" template-func.sh > "${LIB_DIR}/${FUNCTION_NAME}.sh"
+  LIB_DIR=$(readlink -ev ./lib)
+  FUNCTION_NAME="func_${FUNCTION_NAME}"
+  FUNCTION_PATH="${LIB_DIR}/${FUNCTION_NAME}.sh"
+  if [ ! -f "${FUNCTION_PATH}" ]; then
+    sed "s/FUNCTION_NAME/${FUNCTION_NAME}/" template-func.sh > "${FUNCTION_PATH}"
+    echo "Created: ${LIB_DIR}/${FUNCTION_NAME}.sh"
+  else
+    echo "Warning: "${FUNCTION_PATH}" already exists. Aborted!"
+  fi
 
-# Display result.
-echo "Created: ${LIB_DIR}/${FUNCTION_NAME}.sh"
+
+
