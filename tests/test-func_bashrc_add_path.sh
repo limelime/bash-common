ae41a3e43829c7f5fea6cebbb019ee779b8e703d
limelime <limelime@users.noreply.github.com>
2017-02-20 17:41:14 -0500

    Add test add path.

---
+++ tests/test-func_bashrc_add_path.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# Description: Test func_bashrc_add_path.
+
+. assert.sh
+
+# Test empty parameter.
+  assert_raises "func_bashrc_add_path" 1
+
+# Test add path.
+  PATH_UNIQ="${HOME}/78974651"
+  mkdir -p "${PATH_UNIQ}"
+  assert "func_bashrc_add_path ${HOME}/78974651" '~/.bashrc: Added: PATH=$PATH:/root/78974651.'
+  sed -i 's/.*78974651.*//' ~/.bashrc
+
+# Test invalid path.
+  assert_raises "func_bashrc_add_path ${HOME}/someInvalidPath9653" 1
+  
+assert_end func_bashrc_add_path
