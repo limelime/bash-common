ed5d45a498f2cb19dc9763d424c224c7ba1178e4
limelime <limelime@users.noreply.github.com>
2017-02-20 17:41:35 -0500

    Fix: Return error if directory not found.

--- lib/func_bashrc_add_path.sh
+++ lib/func_bashrc_add_path.sh
@@ -4,6 +4,10 @@
 function func_bashrc_add_path()
 { 
   local ENV_PATH=$1
+  if [ ! -d "${ENV_PATH}" ]; then
+    echo "Error: "${ENV_PATH}": no such directory. Aborted!"
+    return 1;
+  fi    
   ENV_PATH=$(readlink -ev "${ENV_PATH}")
   ENV_PATH="PATH=\$PATH:${ENV_PATH}"
   func_bashrc_add "${ENV_PATH}"
