6e874446f125736884ae8898920355cd328d1b4b
limelime <limelime@users.noreply.github.com>
2017-02-20 16:22:19 -0500

    Reorder.

--- tests/test-func_cmd_exists.sh
+++ tests/test-func_cmd_exists.sh
@@ -4,10 +4,8 @@ set -e
 
 . assert.sh
 
-
-assert_raises "func_cmd_exists bash"
 assert_raises "func_cmd_exists" 1
 assert_raises "func_cmd_exists invalidCommand" 1
-
+assert_raises "func_cmd_exists bash"
 
 assert_end func_cmd_exists
