c12b6abe41773db4e772838d4513a45a8fa4302f
limelime <limelime@users.noreply.github.com>
2017-03-03 11:15:30 -0500

    source sizeof_kb.

--- src-init-load.sh
+++ src-init-load.sh
@@ -2,7 +2,7 @@
 #set -e # Don't enable this statement. Otherwise, command not found will close the terminal.
 # Description: Load all *.sh from source lib/
 
-LIB_DIR=$(readlink -ev ./lib)
+#LIB_DIR=$(readlink -ev ./lib)
 #source <(cat ${LIB_DIR}/*.sh) # Cause an error to close current shell.
 #find "${LIB_DIR}" -maxdepth 1 -name 'func_*.sh' -execdir echo "sourced ${LIB_DIR}/{}" \; -execdir /bin/bash -c "source ${LIB_DIR}/{}" \; 
 
@@ -14,3 +14,4 @@ source lib/func_cmd_exists.sh
 source lib/func_dir_get_executing_path.sh
 source lib/func_id.sh
 source lib/func_link.sh
+source lib/func_sizeof_kb.sh
