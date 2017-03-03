4a65904b68058f95e76594e0fdbd0ba5de6647c0
limelime <limelime@users.noreply.github.com>
2017-03-03 11:29:45 -0500

    Add clarification.

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ mkdir -p "${SCRIPTS_DIR}"
 SCRIPTS_DIR=$(readlink -ev "${SCRIPTS_DIR}")
 
 # Copy to target destination directory.
-  yes | cp -a ${LIB_DIR}/* "${SCRIPTS_DIR}"
+  yes | cp -av ${LIB_DIR}/* "${SCRIPTS_DIR}"
 
 # Add in .bashrc
   while IFS='' read -r LINE || [[ -n "${LINE}" ]]; do
@@ -17,4 +17,5 @@ SCRIPTS_DIR=$(readlink -ev "${SCRIPTS_DIR}")
   done < <( find "${SCRIPTS_DIR}" -type f -name '*.sh')
 
 # Display what to do next.
-echo "Run: source ${HOME}/.bashrc"
+echo "###########################"
+echo "To use now, run: source ${HOME}/.bashrc"
