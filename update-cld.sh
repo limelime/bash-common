9f52c5a8e6887aa48a7223acb90ee1a57f7278cc
limelime <limelime@users.noreply.github.com>
2017-05-08 14:33:26 -0400

    Fix update path.

--- update-cld.sh
+++ update-cld.sh
@@ -2,6 +2,6 @@
 set -e
 # Description: Update scripts to Cust-Live-Deb
 
-src_dir=$(readlink -ev .)
-dest_dir=$(readlink -ev "/media/master/github/cust-live-deb/scripts/repository/inst-min-con-bash-scripts")
+src_dir=$(readlink -ev lib)
+dest_dir=$(readlink -ev "/media/master/github/firstboot")
 rsync -rv --exclude=.git "${src_dir}" "${dest_dir}"
